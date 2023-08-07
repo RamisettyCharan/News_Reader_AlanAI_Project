076af14c14b7d21972cc16429a79888e08b228cd
RamisettyCharan <139998621+RamisettyCharan@users.noreply.github.com>
2023-08-07 10:44:20 +0530

    Printing NewCards

--- Basic_example_for_AI_assistant.js
+++ Basic_example_for_AI_assistant.js
@@ -9,18 +9,23 @@ let savedArticles = [];
 intent('Give me the news from $(source* (.*))', (p) => {
     let NEWS_API_URL = `https://newsapi.org/v2/top-headlines?apiKey=${API_KEY}`;
     
-    
+    console.log(NEWS_API_URL);
     if (p.source.value) {
         NEWS_API_URL = `${NEWS_API_URL}&sources=${p.source.value.toLowerCase().split(" ").join('-')}`;
-    }
 
-    api.request(NEWS_API_URL, (error, response, body) => {
+    }
+    
+    const headers = {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36' // Replace 'Your-User-Agent-Name' with a name for your application
+    };
+    
+    api.request(NEWS_API_URL, {headers}, (error, response, body) => {
         if (error) {
             console.error('Error:', error);
             p.play('Sorry, there was an error while fetching the news.');
             return;
         }
-
+        console.log(body);
         const data = JSON.parse(body);
 
         if (!data.articles || data.articles.length === 0) {
@@ -33,6 +38,6 @@ intent('Give me the news from $(source* (.*))', (p) => {
         savedArticles = articles;
 
         p.play({ command: 'newHeadlines', articles });
-        p.play(`Here are the (latest|recent) ${p.source.value} news.`);
+        p.play(`Here are the (latest|recent) ${p.source.value}.`);
     });
 });
